c2c5542a26804c1da0a4cf89a3c7a674a4e7cc31
ralucabaksai <62606434+ralucabaksai@users.noreply.github.com>
2020-08-01 15:39:50 +0300

    Updates

---
+++ precache-manifest.3f38ac53ab8ad2aab28ffe067c0d34a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebb5a35e7e92e7bfb3bfbecbce8dbb4",
+    "url": "/Infinite-Scrolling-React/index.html"
+  },
+  {
+    "revision": "aec5b96b5ac37b9838c7",
+    "url": "/Infinite-Scrolling-React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "507800c62a5f38f2504f",
+    "url": "/Infinite-Scrolling-React/static/css/main.9fe0aa95.chunk.css"
+  },
+  {
+    "revision": "aec5b96b5ac37b9838c7",
+    "url": "/Infinite-Scrolling-React/static/js/2.95d2b978.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Infinite-Scrolling-React/static/js/2.95d2b978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507800c62a5f38f2504f",
+    "url": "/Infinite-Scrolling-React/static/js/main.5040b185.chunk.js"
+  },
+  {
+    "revision": "9d57f8efd4dd4122f532",
+    "url": "/Infinite-Scrolling-React/static/js/runtime-main.fddc0d7e.js"
+  }
+]);
